068e0496a71238093ffddbc06e72d4228a597861
Abhinay Singh <abhinay.anarayn@dbalounge.com>
2024-03-06 15:30:47 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,18 @@
 const nextConfig = {
     env: {
         BASE_URL: "http://localhost:8080/eNach"
-    }
+    },
+
+    experimental: {
+        serverActions: {
+            allowedOrigins: [
+                'localhost:3000',
+                '127.0.0.1:5500',
+                '144.24.96.140',
+                '*',
+            ]
+        }
+    },
 }
 
 module.exports = nextConfig
