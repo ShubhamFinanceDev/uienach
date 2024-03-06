5bc4e61f6ca4d2a5d004a22412759ac611f4bb5c
Abhinay Singh <abhinay.anarayn@dbalounge.com>
2024-03-06 16:51:21 +0530

    base url update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BASE_URL: "http://localhost:8080/eNach"
+        BASE_URL: "http://144.24.96.140:8081/eNach"
     },
 
     experimental: {
