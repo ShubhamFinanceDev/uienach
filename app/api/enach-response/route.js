141196db2a3cca0251b0ffc005fd15edb2789c6b
Abhinay Singh <abhinay.anarayn@dbalounge.com>
2024-03-06 12:52:03 +0530

    remove search params

--- app/api/enach-response/route.js
+++ app/api/enach-response/route.js
@@ -14,7 +14,7 @@ export async function POST(req) {
         formObjectString = formObjectString.join("&")
         // java api call
 
-        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/client/success?${formObjectString}`)
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/client/success`)
     } catch (error) {
         console.error('Error processing form data:', error);
         return NextResponse.json({ error: error })
