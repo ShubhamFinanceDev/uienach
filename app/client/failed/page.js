959d7be3e1e23208e6375e043f2a06f791a17614
Manish Sharma <manish.sharma@dbalounge.com>
2024-03-08 15:21:39 +0530

    sucess page and failed page style name:manish sharma date:08/03/2024

--- app/client/failed/page.js
+++ app/client/failed/page.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Failed = ({ searchParams }) => {
     return (
-        <div>
-            <h1>Failed page</h1>
-            <p>Response: {JSON.stringify(searchParams)}</p>
+
+        <div className='container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+            <div style={{ textAlign: 'center' }}>
+                <h1 style={{ color: 'red', fontSize: '36px' }}>Failed </h1>
+                <p>Enach registration failed due to {JSON.stringify(searchParams)}</p>
+            </div>
         </div>
+
     )
 }
 
--- app/client/success/page.js
+++ app/client/success/page.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Success = ({ searchParams }) => {
     return (
-        <div>
-            <h1>Success page</h1>
-            <p>Response: {JSON.stringify(searchParams)}</p>
+        <div className='container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+            <h1 style={{ color: 'green', fontSize: '36px' }}>Success</h1>
+            <p>Enach registration successfully.</p>
         </div>
-    )
+    );
 }
 
-export default Success
+export default Success;
