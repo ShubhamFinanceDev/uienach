8e013f286ade9fdd3a5d0db7973b62fc01b08c23
kajalthakur99 <kajal.01@dbalounge.com>
2024-07-11 16:12:44 +0530

    adding alert

--- utils/ErrorHandler.js
+++ utils/ErrorHandler.js
@@ -1,8 +1,5 @@
 const ErrorHandler = (error) => {
-    console.error(error)
-    if (error?.message && !error?.response?.status == 401) {
-        alert(error.message)
-    }
+    alert(error.message)
     // snackbar(error?.response?.data?.msg || error?.message, "error")
 
 }
