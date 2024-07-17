55c84bc48243ee82afbb452b8674b5b183904877
manish sharma <manish.sharma@dbalounge.com>
2024-07-17 12:26:52 +0530

    amount add emandate api and success and error  message change and application no send in customerrefrance2

--- app/client/failed/page.js
+++ app/client/failed/page.js
@@ -6,7 +6,7 @@ const Failed = ({ searchParams }) => {
     return (
         <div className='container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
             <div style={{ textAlign: 'center' }}>
-                <h1 style={{ color: 'red', fontSize: '36px' }}>Enach registration has been failed.</h1>
+                <h1 style={{ color: 'red', fontSize: '36px' }}>Enach registration has been failed for {mandateType} of loan aomunt {amount}.</h1>
                 <p className='mt-3'>for application No {applicationNo}, reason is {reason}.</p>
 
             </div>
--- app/client/success/page.js
+++ app/client/success/page.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Success = ({ searchParams }) => {
-    const { applicationNo = "", MsgId = "" } = searchParams
+    const { applicationNo = "", MsgId = "", mandateType="", amount=""} = searchParams
     return (
         <div className='container success-container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
 
             <h1
                 style={{ color: 'green', fontSize: '36px' }}
             >
-                Enach registration successfully completed.
+                Enach registration successfully completed for {mandateType} of loan aomunt {amount}.
             </h1>
             <p className='mt-3'>Reference transaction No. {MsgId} for application No. {applicationNo}.</p>
         </div>
