e2c50e5f4ed5c33959b277c7440f9e313368e26a
manish sharma <manish.sharma@dbalounge.com>
2024-07-18 10:59:38 +0530

    correct meaning

--- app/client/success/page.js
+++ app/client/success/page.js
@@ -6,9 +6,9 @@ const Success = ({ searchParams }) => {
         <div className='container success-container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
 
             <h1
-                style={{ color: 'green', fontSize: '36px' }}
+                style={{ color: 'green', fontSize: '25px' }}
             >
-                Enach registration successfully completed for {mandateType} of loan aomunt {amount}.
+                Enach registration successfully completed for {mandateType} of loan amount {amount}.
             </h1>
             <p className='mt-3'>Reference transaction No. {MsgId} for application No. {applicationNo}.</p>
         </div>
