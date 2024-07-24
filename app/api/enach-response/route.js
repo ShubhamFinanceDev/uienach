2ec001626fb3d788aa0dfd43b17605d1497ddcb6
manish sharma <manish.sharma@dbalounge.com>
2024-07-24 14:57:06 +0530

    add umrn and return bankName etc

--- app/api/enach-response/route.js
+++ app/api/enach-response/route.js
@@ -15,7 +15,7 @@ export async function POST(req) {
         const jsonString = formObject.MandateRespDoc.replace(/'/g, '"');
         const parsedObject = JSON.parse(jsonString);
 
-        const { MsgId, Status = "Something went wrong!", Errors = {}, RefId="" } = parsedObject;
+        const { MsgId, Status = "Something went wrong!", Errors = {}, RefId="", Filer10="" } = parsedObject;
 
 
         let errorObject = []
@@ -29,7 +29,7 @@ export async function POST(req) {
 
         const errorString = errorObj2.join(", ") || ""
         
-        const { data: { applicationNo = "" , mandateType="", amount=""} } = await axios.put(api.enachPaymentStatus(MsgId), { transactionStatus: Status, errorMessage : errorString,  refrenceId:RefId});
+        const { data: { applicationNo = "" , mandateType="", amount=""} } = await axios.put(api.enachPaymentStatus(MsgId), { transactionStatus: Status, errorMessage : errorString,  refrenceId:RefId, umrn:Filer10});
         const transactionQuery = `applicationNo=${applicationNo}&MsgId=${MsgId}&mandateType=${mandateType}&amount=${amount}`
 
         if (Status === 'Failed') {
